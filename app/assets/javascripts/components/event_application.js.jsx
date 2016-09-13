f286a873416310e73e39e1f0de1cb2dcd2592c6b
Stas Zelenko <sharcik14@gmail.com>
2016-09-13 11:36:16 +0300

    change EventApplication

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -1,4 +1,22 @@
 var EventApplication = React.createClass({
+  getInitialState: function() {
+    return {events: [] };
+  },
+  componentDidMount: function() {
+    this.getDataFromApi();
+  },
+  getDataFromApi: function() {
+    var self = this;
+    $.ajax({
+      url: '/api/events',
+      success: function(data) {
+        self.setState({ events:data });
+      },
+      error: function(xhr,status,error){
+        alert('Cannot get data from API: ',error);
+      }
+    })
+  },
   render: function() {
     return (
       <div className="container">
@@ -8,10 +26,51 @@ var EventApplication = React.createClass({
         </div>
         <div className="row">
           <div className="col-md-12">
-            <EventTable />
+            <EventTable events={this.state.events}/>
           </div>
         </div>
       </div>
+    )
+  }
+});
+
+
+var EventTable = React.createClass({
+  render: function() {
+    return (
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th className="col-md-3">Name</th>
+            <th className="col-md-2">Date</th>
+            <th className="col-md-3">Place</th>
+            <th className="col-md-4">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Event name="new_event" />
+        </tbody>
+      </table>
+    )
+  }
+});
+
+var Event = React.createClass({
+  propTypes:{
+    name: React.propTypes.string,
+    event_date: React.propTypes.string,
+    place: React.propTypes.string,
+    description: React.propTypes.string,
+  },
+  render: function() {
+    var event = this.props.event;
+    return(
+      <tr>
+        <td>{event.name}</td>
+        <td>{event.event_date}</td>
+        <td>{event.place}</td>
+        <td>{event.description}</td>
+      </tr>
     )
   }
 });
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+1.upto(10) do |i|
+  Event.create(name: "Event #{i}",
+               description: "It,s sample event with number #{i}",
+               event_date: Date.today + rand(3).month,
+               place: "random place number #{i}")
+end
